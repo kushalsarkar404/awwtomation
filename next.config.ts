def4d5370da6e9abc2521a6f65f4b116d3537c65
Sarkar KUSHAL <macbookpron13@CALCULATOR.local>
2025-06-05 13:00:32 -0400

    seo improved

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  redirects: async () => [
-    {
-      source: "/:path*",
-      destination: "https://awwtomation.com/:path*",
-      permanent: true,
-    },
-  ],
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
