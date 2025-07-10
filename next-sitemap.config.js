93c3a1dd32e80dde0f8907b93de10c8bbaa24c04
Sarkar KUSHAL <macbookpron13@CALCULATOR.local>
2025-07-10 11:59:51 -0400

    sitemap changed

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,18 +3,18 @@ module.exports = {
   siteUrl: 'https://www.awwtomation.com',
   generateRobotsTxt: true,
   changefreq: 'weekly',
-  priority: 0.7,
+  priority: 1,
   sitemapSize: 5000,
   exclude: ['/api/*'],
   // Explicitly include your important pages
   additionalPaths: async (config) => [
     await config.transform(config, '/'),
-    await config.transform(config, '/about'),
     await config.transform(config, '/services/blog-automation'),
     await config.transform(config, '/services/seo-automation'),
     await config.transform(config, '/services/crm-automation'),
     await config.transform(config, '/services/social-media-automation'),
     await config.transform(config, '/services/email-marketing-automation'),
+    await config.transform(config, '/about'),
     await config.transform(config, '/blog'),
   ]
 }
